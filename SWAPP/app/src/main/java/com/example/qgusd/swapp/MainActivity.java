554ebb2f253a0e78c04dcb265fddbeaf4e1224c2
qrlagusdn <qrlagusdn@naver.com>
2018-10-26 17:00:10 +0900

    start2

--- SWAPP/app/src/main/java/com/example/qgusd/swapp/MainActivity.java
+++ SWAPP/app/src/main/java/com/example/qgusd/swapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.qgusd.swapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btn_blue = (Button)findViewById(R.id.btn_blue);
+        Button btn_regi = (Button)findViewById(R.id.btn_regi);
+
+        btn_blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BluetoothActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_regi.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
